f5820f05ba881b6c1ccb01aa5e0a6a50cc91d570
Ray Wang <ray.wang@techcertain.com>
2019-10-04 11:18:39 +1300

    appsetting variable

---
+++ TechCertain.Domain.Entities/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechCertain.Domain.Entities
+{
+    public class AppSettings
+    {
+        public string CarJamEndpoint { get; set; }
+        public string CarJamApiKey { get; set; }
+
+    }
+}
