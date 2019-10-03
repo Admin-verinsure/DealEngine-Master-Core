c2573217833b4e860b0284969d27710ace571c03
James Dillon <_james@techcertain.com>
2019-10-04 10:08:04 +1300

    Adding factory services

---
+++ DealEngine.Infrastructure.AppInitialize/Factories/FactoriesExtenstions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TechCertain.Domain.Entities.Abstracts;
+using TechCertain.Domain.Interfaces;
+using TechCertain.Domain.Services.Factories;
+
+namespace DealEngine.Infrastructure.AppInitialize
+{
+    public static class FactoriesExtenstions
+    {
+        public static IServiceCollection AddFactories(this IServiceCollection services)
+        {
+            services.AddSingleton<IEntityFactory, InformationItemFactory>();
+            services.AddSingleton<>();
+            services.AddSingleton<>()
+                services.AddSingleton<>()
+                services.AddSingleton<>()
+
+            return services;
+        }
+    }
+
+    
+}
