7befdb841366031487f6d78e6420691cde74da70
Ray Wang <ray.wang@techcertain.com>
2019-11-06 12:49:10 +1300

    aray local

--- TechCertain.WebUI/Controllers/ProgrammeController.cs
+++ TechCertain.WebUI/Controllers/ProgrammeController.cs
@@ -469,7 +469,7 @@ namespace TechCertain.WebUI.Controllers
 
             var xmlPayload = eGlobalSerializer.SerializePolicy(programme, user, _unitOfWork);
 
-            var byteResponse = await _httpClientService.CreateEGlobalInvoice(xmlPayload).ConfigureAwait(true);
+            var byteResponse = await _httpClientService.CreateEGlobalInvoice(xmlPayload);
 
             eGlobalSerializer.DeSerializeResponse(byteResponse, programme, user, _unitOfWork);
 
