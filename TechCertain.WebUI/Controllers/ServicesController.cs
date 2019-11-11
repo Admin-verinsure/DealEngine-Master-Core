07076a86c3e1489abff76535cafccfa2cf239756
Ray Wang <ray.wang@techcertain.com>
2019-11-12 09:10:27 +1300

    new client programme changes

--- TechCertain.WebUI/Controllers/ServicesController.cs
+++ TechCertain.WebUI/Controllers/ServicesController.cs
@@ -2550,6 +2550,7 @@ namespace TechCertain.WebUI.Controllers
                     sheet.Boats.Add(vessel);
                     organisation.OrganisationalUnits.Add(ou);
                     clientProgramme.BrokerContactUser = programme.BrokerContactUser;
+                    clientProgramme.ClientProgrammeMembershipNumber = membershipNumber;
                     sheet.ClientInformationSheetAuditLogs.Add(new AuditLog(user, sheet, null, "Quick Quote Consuming Process Completed"));
                     try
                     {
