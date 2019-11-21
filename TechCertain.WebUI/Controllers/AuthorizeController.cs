111667dfee2d57c2a8e9312368616e06facb4d17
James <James@techcertain.com>
2019-11-21 16:10:12 +1300

    hot fixes to identity

--- TechCertain.WebUI/Controllers/AuthorizeController.cs
+++ TechCertain.WebUI/Controllers/AuthorizeController.cs
@@ -45,6 +45,7 @@ namespace TechCertain.WebUI.Controllers
         {
             var user = await CurrentUser();
             var userRoleList = await _userRoleService.GetRolesByOrganisation(user.PrimaryOrganisation);
+
             var userList = await _userService.GetAllUsers();
             var roleList = new List<IdentityRole>();
             var organisationList = await _organisationService.GetAllOrganisations();
@@ -63,15 +64,22 @@ namespace TechCertain.WebUI.Controllers
             model.ClaimList = claimList;
             model.Organisations = organisationList;
 
-            if (userRoleList.Count != 0)
+            if (user.PrimaryOrganisation.IsTC)
             {
-                foreach(var userRole in userRoleList)
-                {
-                    roleList.Add(userRole.IdentityRole);
-                }
-
-                model.RoleList = roleList;
+                roleList = await _roleManager.Roles.ToListAsync();
             }
+            else
+            {
+                if (userRoleList.Count != 0)
+                {
+                    foreach (var userRole in userRoleList)
+                    {
+                        roleList.Add(userRole.IdentityRole);
+                    }                    
+                }
+            }
+            
+            model.RoleList = roleList;
 
             if (userList.Count != 0)
             {
@@ -82,15 +90,15 @@ namespace TechCertain.WebUI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddRole(string RoleName, string[] Claims, Guid OrganisationId)
+        public async Task<IActionResult> AddRole(string RoleName, string[] Claims, string OrganisationId)
         {
             var user = await CurrentUser();
             var isRole = await _roleManager.RoleExistsAsync(RoleName);
             var organisation = user.PrimaryOrganisation;
 
-            if(OrganisationId != null)
+            if(!string.IsNullOrEmpty(OrganisationId))
             {
-                organisation = await _organisationService.GetOrganisation(OrganisationId);
+                organisation = await _organisationService.GetOrganisation(Guid.Parse(OrganisationId));
             }
 
             if (!isRole)
