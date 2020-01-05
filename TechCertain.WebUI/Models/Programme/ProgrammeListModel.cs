203b165eb4c9a8d581e8b348c0af20a3a8e0243a
James <James@techcertain.com>
2020-01-06 09:57:17 +1300

    Initial Programme Wizard Commmit

---
+++ TechCertain.WebUI/Models/Programme/ProgrammeListModel.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+
+namespace TechCertain.WebUI.Models.Programme
+{
+
+	public class ProgrammeListModel : BaseViewModel
+	{
+		public IList<Domain.Entities.Programme> Programmes { get; set; }
+    }
+}
+
