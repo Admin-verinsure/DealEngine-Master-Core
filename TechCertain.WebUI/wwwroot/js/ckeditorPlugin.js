3dd33676c8b468bc8ae744498a985064ab2b6f01
tcnathan <nathan@techcertain.com>
2020-02-03 13:29:44 +1300

    Put plugin code into js file and fixed createdoc

---
+++ TechCertain.WebUI/wwwroot/js/ckeditorPlugin.js
@@ -0,0 +1,8 @@
+function selectImg(img) {
+    var filename = img.name;
+    $('#selImg').val(filename);
+}
+function importImg() {
+    var filename = $('#selImg').val();
+    $('#importImg').val(filename);
+}
