0817af6970fad36e6a007874433af674639e3306
Ray Wang <ray.wang@techcertain.com>
2020-05-18 07:57:31 +1200

    NZFSG UWM

---
+++ DealEngine.Services.Impl/UnderwritingModuleServices/NZFSGPLUWModule.cs
@@ -0,0 +1,199 @@
+﻿using DealEngine.Services.Interfaces;
+using DealEngine.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DealEngine.Services.Impl.UnderwritingModuleServices
+{
+    public class NZFSGPLUWModule : IUnderwritingModule
+    {
+        public string Name { get; protected set; }
+
+        public NZFSGPLUWModule()
+        {
+            Name = "NZFSG_PL";
+        }
+
+        public bool Underwrite(User CurrentUser, ClientInformationSheet informationSheet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Underwrite(User underwritingUser, ClientInformationSheet informationSheet, Product product, string reference)
+        {
+            ClientAgreement agreement = GetClientAgreement(underwritingUser, informationSheet, informationSheet.Programme, product, reference);
+            Guid id = agreement.Id;
+
+            if (agreement.ClientAgreementRules.Count == 0)
+                foreach (var rule in product.Rules.Where(r => !string.IsNullOrWhiteSpace(r.Name)))
+                    agreement.ClientAgreementRules.Add(new ClientAgreementRule(underwritingUser, rule, agreement));
+
+            if (agreement.ClientAgreementEndorsements.Count == 0)
+                foreach (var endorsement in product.Endorsements.Where(e => !string.IsNullOrWhiteSpace(e.Name)))
+                    agreement.ClientAgreementEndorsements.Add(new ClientAgreementEndorsement(underwritingUser, endorsement, agreement));
+
+            if (agreement.ClientAgreementTerms.Where(ct => ct.SubTermType == "PL" && ct.DateDeleted == null) != null)
+            {
+                foreach (ClientAgreementTerm plterm in agreement.ClientAgreementTerms.Where(ct => ct.SubTermType == "PL" && ct.DateDeleted == null))
+                {
+                    plterm.Delete(underwritingUser);
+                }
+            }
+
+            //IDictionary<string, decimal> rates = BuildRulesTable(agreement, "pl1millimitpremium", "pl2millimitpremium");
+
+            //Create default referral points based on the clientagreementrules
+            if (agreement.ClientAgreementReferrals.Count == 0)
+            {
+                foreach (var clientagreementreferralrule in agreement.ClientAgreementRules.Where(cr => cr.RuleCategory == "uwreferral" && cr.DateDeleted == null))
+                    agreement.ClientAgreementReferrals.Add(new ClientAgreementReferral(underwritingUser, agreement, clientagreementreferralrule.Name, clientagreementreferralrule.Description, "", clientagreementreferralrule.Value, clientagreementreferralrule.OrderNumber));
+            }
+            else
+            {
+                foreach (var clientagreementreferral in agreement.ClientAgreementReferrals.Where(cref => cref.DateDeleted == null))
+                    clientagreementreferral.Status = "";
+            }
+
+            int agreementperiodindays = 0;
+            agreementperiodindays = (agreement.ExpiryDate - agreement.InceptionDate).Days;
+
+            agreement.QuoteDate = DateTime.UtcNow;
+
+            string strretrodate = "";
+            if (agreement.ClientInformationSheet.PreRenewOrRefDatas.Count() > 0)
+            {
+                foreach (var preRenewOrRefData in agreement.ClientInformationSheet.PreRenewOrRefDatas)
+                {
+                    if (preRenewOrRefData.DataType == "preterm")
+                    {
+                        if (!string.IsNullOrEmpty(preRenewOrRefData.GLRetro))
+                        {
+                            strretrodate = preRenewOrRefData.GLRetro;
+                        }
+
+                    }
+                    if (preRenewOrRefData.DataType == "preendorsement" && preRenewOrRefData.EndorsementProduct == "GL")
+                    {
+                        if (agreement.ClientAgreementEndorsements.FirstOrDefault(cae => cae.Name == preRenewOrRefData.EndorsementTitle) == null)
+                        {
+                            ClientAgreementEndorsement clientAgreementEndorsement = new ClientAgreementEndorsement(underwritingUser, preRenewOrRefData.EndorsementTitle, "Exclusion", product, preRenewOrRefData.EndorsementText, 130, agreement);
+                            agreement.ClientAgreementEndorsements.Add(clientAgreementEndorsement);
+                        }
+                    }
+                }
+            }
+
+            int TermLimit10mil = 10000000;
+            decimal TermPremium10mil = 0m;
+            decimal TermBrokerage10mil = 0m;
+
+            int TermExcess = 0;
+            TermExcess = 250;
+
+            TermBrokerage10mil = TermPremium10mil * agreement.Brokerage;
+
+            ClientAgreementTerm termpl10millimitoption = GetAgreementTerm(underwritingUser, agreement, "PL", TermLimit10mil, TermExcess);
+            termpl10millimitoption.TermLimit = TermLimit10mil;
+            termpl10millimitoption.Premium = TermPremium10mil;
+            termpl10millimitoption.Excess = TermExcess;
+            termpl10millimitoption.BrokerageRate = agreement.Brokerage;
+            termpl10millimitoption.Brokerage = TermBrokerage10mil;
+            termpl10millimitoption.DateDeleted = null;
+            termpl10millimitoption.DeletedBy = null;
+
+            //Referral points per agreement
+
+
+            //Update agreement status
+            if (agreement.ClientAgreementReferrals.Where(cref => cref.DateDeleted == null && cref.Status == "Pending").Count() > 0)
+            {
+                agreement.Status = "Referred";
+            }
+            else
+            {
+                agreement.Status = "Quoted";
+            }
+
+            agreement.ProfessionalBusiness = "";
+            agreement.TerritoryLimit = "New Zealand";
+            agreement.Jurisdiction = "New Zealand";
+
+            string auditLogDetail = "NZFSG PL UW created/modified";
+            AuditLog auditLog = new AuditLog(underwritingUser, informationSheet, agreement, auditLogDetail);
+            agreement.ClientAgreementAuditLogs.Add(auditLog);
+
+            return true;
+
+        }
+
+        ClientAgreement GetClientAgreement(User currentUser, ClientInformationSheet informationSheet, ClientProgramme programme, Product product, string reference)
+        {
+            ClientAgreement clientAgreement = programme.Agreements.FirstOrDefault(a => a.Product != null && a.Product.Id == product.Id);
+            ClientAgreement previousClientAgreement = null;
+            if (clientAgreement == null)
+            {
+                DateTime inceptionDate = (product.DefaultInceptionDate > DateTime.MinValue) ? product.DefaultInceptionDate : DateTime.UtcNow;
+                DateTime expiryDate = (product.DefaultExpiryDate > DateTime.MinValue) ? product.DefaultExpiryDate : DateTime.UtcNow.AddYears(1);
+
+                if (informationSheet.IsChange) //change agreement to keep the original inception date and expiry date
+                {
+                    if (informationSheet.PreviousInformationSheet != null)
+                    {
+                        previousClientAgreement = informationSheet.PreviousInformationSheet.Programme.Agreements.FirstOrDefault(prea => prea.Product != null && prea.Product.Id == product.Id);
+                        if (previousClientAgreement != null)
+                        {
+                            inceptionDate = previousClientAgreement.InceptionDate;
+                            expiryDate = previousClientAgreement.ExpiryDate;
+                        }
+                    }
+                }
+                clientAgreement = new ClientAgreement(currentUser, informationSheet.Owner.Name, inceptionDate, expiryDate, product.DefaultBrokerage, product.DefaultBrokerFee, informationSheet, product, reference);
+                if (product.IsMasterProduct)
+                {
+                    clientAgreement.MasterAgreement = true;
+                }
+                else
+                {
+                    clientAgreement.MasterAgreement = false;
+                }
+                clientAgreement.PreviousAgreement = previousClientAgreement;
+                programme.Agreements.Add(clientAgreement);
+                clientAgreement.Status = "Quoted";
+            }
+            else
+            {
+                clientAgreement.DeletedBy = null;
+                clientAgreement.DateDeleted = null;
+            }
+            return clientAgreement;
+        }
+
+        ClientAgreementTerm GetAgreementTerm(User CurrentUser, ClientAgreement agreement, string subTerm, int limitoption, decimal excessoption)
+        {
+            ClientAgreementTerm term = agreement.ClientAgreementTerms.FirstOrDefault(t => t.SubTermType == subTerm && t.DateDeleted != null && t.TermLimit == limitoption && t.Excess == excessoption);
+
+            if (term == null)
+            {
+                term = new ClientAgreementTerm(CurrentUser, 0, 0m, 0m, 0m, 0m, 0m, agreement, subTerm);
+                agreement.ClientAgreementTerms.Add(term);
+            }
+
+            return term;
+        }
+
+        IDictionary<string, decimal> BuildRulesTable(ClientAgreement agreement, params string[] names)
+        {
+            var dict = new Dictionary<string, decimal>();
+
+            foreach (string name in names)
+                dict[name] = Convert.ToDecimal(agreement.ClientAgreementRules.FirstOrDefault(r => r.Name == name).Value);
+
+            return dict;
+        }
+
+
+
+
+    }
+}
---
+++ DealEngine.Services.Impl/UnderwritingModuleServices/NZFSGSLUWModule.cs
@@ -0,0 +1,212 @@
+﻿using DealEngine.Services.Interfaces;
+using DealEngine.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DealEngine.Services.Impl.UnderwritingModuleServices
+{
+    public class NZFSGSLUWModule : IUnderwritingModule
+    {
+        public string Name { get; protected set; }
+
+        public NZFSGSLUWModule()
+        {
+            Name = "NZFSG_SL";
+        }
+
+        public bool Underwrite(User CurrentUser, ClientInformationSheet informationSheet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Underwrite(User underwritingUser, ClientInformationSheet informationSheet, Product product, string reference)
+        {
+            ClientAgreement agreement = GetClientAgreement(underwritingUser, informationSheet, informationSheet.Programme, product, reference);
+            Guid id = agreement.Id;
+
+            if (agreement.ClientAgreementRules.Count == 0)
+                foreach (var rule in product.Rules.Where(r => !string.IsNullOrWhiteSpace(r.Name)))
+                    agreement.ClientAgreementRules.Add(new ClientAgreementRule(underwritingUser, rule, agreement));
+
+            if (agreement.ClientAgreementEndorsements.Count == 0)
+                foreach (var endorsement in product.Endorsements.Where(e => !string.IsNullOrWhiteSpace(e.Name)))
+                    agreement.ClientAgreementEndorsements.Add(new ClientAgreementEndorsement(underwritingUser, endorsement, agreement));
+
+            if (agreement.ClientAgreementTerms.Where(ct => ct.SubTermType == "SL" && ct.DateDeleted == null) != null)
+            {
+                foreach (ClientAgreementTerm slterm in agreement.ClientAgreementTerms.Where(ct => ct.SubTermType == "SL" && ct.DateDeleted == null))
+                {
+                    slterm.Delete(underwritingUser);
+                }
+            }
+
+            //IDictionary<string, decimal> rates = BuildRulesTable(agreement, "sl250klimitminpremium", "sl500klimitminpremium", "sl1millimitminpremium",
+            //    "sl250klimitunder6employeerate", "sl500klimitunder6employeerate", "sl1millimitunder6employeerate", "sl250klimit6to10employeerate", "sl500klimit6to10employeerate",
+            //    "sl1millimit6to10employeerate", "sl250klimitover10employeerate", "sl500klimitover10employeerate", "sl1millimitover10employeerate");
+
+            //Create default referral points based on the clientagreementrules
+            if (agreement.ClientAgreementReferrals.Count == 0)
+            {
+                foreach (var clientagreementreferralrule in agreement.ClientAgreementRules.Where(cr => cr.RuleCategory == "uwreferral" && cr.DateDeleted == null))
+                    agreement.ClientAgreementReferrals.Add(new ClientAgreementReferral(underwritingUser, agreement, clientagreementreferralrule.Name, clientagreementreferralrule.Description, "", clientagreementreferralrule.Value, clientagreementreferralrule.OrderNumber));
+            }
+            else
+            {
+                foreach (var clientagreementreferral in agreement.ClientAgreementReferrals.Where(cref => cref.DateDeleted == null))
+                    clientagreementreferral.Status = "";
+            }
+
+            int agreementperiodindays = 0;
+            agreementperiodindays = (agreement.ExpiryDate - agreement.InceptionDate).Days;
+
+            agreement.QuoteDate = DateTime.UtcNow;
+
+            string strretrodate = "";
+            if (agreement.ClientInformationSheet.PreRenewOrRefDatas.Count() > 0)
+            {
+                foreach (var preRenewOrRefData in agreement.ClientInformationSheet.PreRenewOrRefDatas)
+                {
+                    if (preRenewOrRefData.DataType == "preterm")
+                    {
+                        if (!string.IsNullOrEmpty(preRenewOrRefData.SLRetro))
+                        {
+                            strretrodate = preRenewOrRefData.SLRetro;
+                        }
+
+                    }
+                    if (preRenewOrRefData.DataType == "preendorsement" && preRenewOrRefData.EndorsementProduct == "SL")
+                    {
+                        if (agreement.ClientAgreementEndorsements.FirstOrDefault(cae => cae.Name == preRenewOrRefData.EndorsementTitle) == null)
+                        {
+                            ClientAgreementEndorsement clientAgreementEndorsement = new ClientAgreementEndorsement(underwritingUser, preRenewOrRefData.EndorsementTitle, "Exclusion", product, preRenewOrRefData.EndorsementText, 130, agreement);
+                            agreement.ClientAgreementEndorsements.Add(clientAgreementEndorsement);
+                        }
+                    }
+                }
+            }
+
+            int TermLimit1mil = 1000000;
+            decimal TermPremium1mil = 0m;
+            decimal TermBrokerage1mil = 0m;
+
+            int TermExcess = 0;
+
+            //Return terms based on the limit options
+
+            TermExcess = 500;
+
+            ClientAgreementTerm termsl1millimitoption = GetAgreementTerm(underwritingUser, agreement, "SL", TermLimit1mil, TermExcess);
+            termsl1millimitoption.TermLimit = TermLimit1mil;
+            termsl1millimitoption.Premium = TermPremium1mil;
+            termsl1millimitoption.Excess = TermExcess;
+            termsl1millimitoption.BrokerageRate = agreement.Brokerage;
+            termsl1millimitoption.Brokerage = TermBrokerage1mil;
+            termsl1millimitoption.DateDeleted = null;
+            termsl1millimitoption.DeletedBy = null;
+
+
+            ////Referral points per agreement
+
+
+            //Update agreement status
+            if (agreement.ClientAgreementReferrals.Where(cref => cref.DateDeleted == null && cref.Status == "Pending").Count() > 0)
+            {
+                agreement.Status = "Referred";
+            }
+            else
+            {
+                agreement.Status = "Quoted";
+            }
+
+            agreement.ProfessionalBusiness = "";
+            string retrodate = agreement.InceptionDate.ToShortDateString();
+            agreement.TerritoryLimit = "New Zealand";
+            agreement.Jurisdiction = "New Zealand";
+            agreement.RetroactiveDate = retrodate;
+            if (!String.IsNullOrEmpty(strretrodate))
+            {
+                agreement.RetroactiveDate = strretrodate;
+            }
+
+            string auditLogDetail = "NZFSG SL UW created/modified";
+            AuditLog auditLog = new AuditLog(underwritingUser, informationSheet, agreement, auditLogDetail);
+            agreement.ClientAgreementAuditLogs.Add(auditLog);
+
+            return true;
+
+        }
+
+        ClientAgreement GetClientAgreement(User currentUser, ClientInformationSheet informationSheet, ClientProgramme programme, Product product, string reference)
+        {
+            ClientAgreement clientAgreement = programme.Agreements.FirstOrDefault(a => a.Product != null && a.Product.Id == product.Id);
+            ClientAgreement previousClientAgreement = null;
+            if (clientAgreement == null)
+            {
+                DateTime inceptionDate = (product.DefaultInceptionDate > DateTime.MinValue) ? product.DefaultInceptionDate : DateTime.UtcNow;
+                DateTime expiryDate = (product.DefaultExpiryDate > DateTime.MinValue) ? product.DefaultExpiryDate : DateTime.UtcNow.AddYears(1);
+
+                if (informationSheet.IsChange) //change agreement to keep the original inception date and expiry date
+                {
+                    if (informationSheet.PreviousInformationSheet != null)
+                    {
+                        previousClientAgreement = informationSheet.PreviousInformationSheet.Programme.Agreements.FirstOrDefault(prea => prea.Product != null && prea.Product.Id == product.Id);
+                        if (previousClientAgreement != null)
+                        {
+                            inceptionDate = previousClientAgreement.InceptionDate;
+                            expiryDate = previousClientAgreement.ExpiryDate;
+                        }
+                    }
+                }
+                clientAgreement = new ClientAgreement(currentUser, informationSheet.Owner.Name, inceptionDate, expiryDate, product.DefaultBrokerage, product.DefaultBrokerFee, informationSheet, product, reference);
+                if (product.IsMasterProduct)
+                {
+                    clientAgreement.MasterAgreement = true;
+                }
+                else
+                {
+                    clientAgreement.MasterAgreement = false;
+                }
+                clientAgreement.PreviousAgreement = previousClientAgreement;
+                programme.Agreements.Add(clientAgreement);
+                clientAgreement.Status = "Quoted";
+            }
+            else
+            {
+                clientAgreement.DeletedBy = null;
+                clientAgreement.DateDeleted = null;
+            }
+            return clientAgreement;
+        }
+
+        ClientAgreementTerm GetAgreementTerm(User CurrentUser, ClientAgreement agreement, string subTerm, int limitoption, decimal excessoption)
+        {
+            ClientAgreementTerm term = agreement.ClientAgreementTerms.FirstOrDefault(t => t.SubTermType == subTerm && t.DateDeleted != null && t.TermLimit == limitoption && t.Excess == excessoption);
+
+            if (term == null)
+            {
+                term = new ClientAgreementTerm(CurrentUser, 0, 0m, 0m, 0m, 0m, 0m, agreement, subTerm);
+                agreement.ClientAgreementTerms.Add(term);
+            }
+
+            return term;
+        }
+
+        IDictionary<string, decimal> BuildRulesTable(ClientAgreement agreement, params string[] names)
+        {
+            var dict = new Dictionary<string, decimal>();
+
+            foreach (string name in names)
+                dict[name] = Convert.ToDecimal(agreement.ClientAgreementRules.FirstOrDefault(r => r.Name == name).Value);
+
+            return dict;
+        }
+
+
+
+
+    }
+}
+
+
+
