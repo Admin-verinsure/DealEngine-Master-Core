faa00682a672dff61cb19b925099846030f5d480
Ashu <ashu@techcertain.com>
2020-06-09 13:23:37 +1200

    unlock reason

---
+++ DealEngine.Domain.Entities/UnlockReason.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DealEngine.Domain.Entities.Abstracts;
+
+
+namespace DealEngine.Domain.Entities
+{
+    public class UnlockReason : EntityBase
+    {
+
+        public UnlockReason() : base (null) { }
+
+        public UnlockReason(User createdBy)
+        : base(createdBy)
+        {
+        }
+        public virtual Guid DealId { get; set; }
+        public virtual string Reason { get; set; }
+
+    }
+}
