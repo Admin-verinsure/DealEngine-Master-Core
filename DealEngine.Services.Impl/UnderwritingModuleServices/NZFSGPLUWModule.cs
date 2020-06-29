aa2f87c8e4b2f2024cefd97caf4bf06960764e76
Ray Wang <ray.wang@techcertain.com>
2020-06-29 13:24:42 +1200

    nzfsg uwm changes and permission change for unbind

--- DealEngine.Services.Impl/UnderwritingModuleServices/NZFSGPLUWModule.cs
+++ DealEngine.Services.Impl/UnderwritingModuleServices/NZFSGPLUWModule.cs
@@ -145,6 +145,8 @@ namespace DealEngine.Services.Impl.UnderwritingModuleServices
                 agreement.RetroactiveDate = strretrodate;
             }
 
+            agreement.InsuredName = informationSheet.Owner.Name;
+
             string auditLogDetail = "NZFSG PL UW created/modified";
             AuditLog auditLog = new AuditLog(underwritingUser, informationSheet, agreement, auditLogDetail);
             agreement.ClientAgreementAuditLogs.Add(auditLog);
