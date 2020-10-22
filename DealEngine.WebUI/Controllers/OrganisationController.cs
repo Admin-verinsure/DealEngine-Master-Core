f469cc5836e526bcfb472d7ee9e2059ae844a410
James <James@techcertain.com>
2020-10-23 09:50:23 +1300

    --1

--- DealEngine.WebUI/Controllers/OrganisationController.cs
+++ DealEngine.WebUI/Controllers/OrganisationController.cs
@@ -331,11 +331,8 @@ namespace DealEngine.WebUI.Controllers
         [HttpPost]
         public async Task<IActionResult> AttachOrganisation(IFormCollection collection)
         {
+            await _userService.GetUserPrimaryOrganisation(DetachOrganisation();
             await _programmeService.AttachOrganisationToClientProgramme(collection);
-
-            //var Programme = await _programmeService.GetProgrammeById(ProgrammeId);
-            //var RemovedOrg = await _organisationService.GetOrganisation(OrganisationId);
-            //AttachOrganisationViewModel model = new AttachOrganisationViewModel(Programme.ClientProgrammes, RemovedOrg);
             return RedirectToAction("Index", "Home");
         }
 
