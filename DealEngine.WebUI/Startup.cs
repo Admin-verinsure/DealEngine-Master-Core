969fdf2a583c300599f0eb129f5fbdb4281eaca4
tcnathan <nathan@techcertain.com>
2021-03-23 15:44:14 +1300

    Content Security Policy

--- DealEngine.WebUI/Startup.cs
+++ DealEngine.WebUI/Startup.cs
@@ -16,6 +16,8 @@ using Microsoft.AspNetCore.Localization;
 using AutoMapper;
 using Newtonsoft.Json;
 using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
 
 namespace DealEngine.WebUI
 {
@@ -62,6 +64,12 @@ namespace DealEngine.WebUI
             //{
             //    options.IdleTimeout = TimeSpan.FromMinutes(60);
             //});
+            services.AddHsts(options =>
+            {
+                options.MaxAge = TimeSpan.FromDays(90);
+                options.IncludeSubDomains = true;
+                options.Preload = true;
+            });
         }
 
         public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -89,8 +97,8 @@ namespace DealEngine.WebUI
             app.UseAuthentication();
             app.UseAuthorization();
             //app.UseSession();
-            //app.UseMiddleware()
-            
+            app.UseMiddleware<SecurityHeadersMiddleware>();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
@@ -101,4 +109,103 @@ namespace DealEngine.WebUI
         }
     }
 
+}
+
+public sealed class SecurityHeadersMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public SecurityHeadersMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public Task Invoke(HttpContext context)
+    {
+        if (context != null)
+        {
+            #region Other Headers that can be used
+            //// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+            //// TODO Change the value depending of your needs
+            //context.Response.Headers.Add("referrer-policy", new StringValues("strict-origin-when-cross-origin"));
+
+            //// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+            //context.Response.Headers.Add("x-content-type-options", new StringValues("nosniff"));
+
+            //// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+            //context.Response.Headers.Add("x-frame-options", new StringValues("DENY"));
+
+            //// https://security.stackexchange.com/questions/166024/does-the-x-permitted-cross-domain-policies-header-have-any-benefit-for-my-websit
+            //context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", new StringValues("none"));
+
+            //// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
+            //context.Response.Headers.Add("x-xss-protection", new StringValues("1; mode=block"));
+
+            //// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Expect-CT
+            //// You can use https://report-uri.com/ to get notified when a misissued certificate is detected
+            //context.Response.Headers.Add("Expect-CT", new StringValues("max-age=0, enforce, report-uri=\"https://example.report-uri.com/r/d/ct/enforce\""));
+
+            //// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+            //// https://github.com/w3c/webappsec-feature-policy/blob/master/features.md
+            //// https://developers.google.com/web/updates/2018/06/feature-policy
+            //// TODO change the value of each rule and check the documentation to see if new features are available
+            //context.Response.Headers.Add("Feature-Policy", new StringValues(
+            //    "accelerometer 'none';" +
+            //    "ambient-light-sensor 'none';" +
+            //    "autoplay 'none';" +
+            //    "battery 'none';" +
+            //    "camera 'none';" +
+            //    "display-capture 'none';" +
+            //    "document-domain 'none';" +
+            //    "encrypted-media 'none';" +
+            //    "execution-while-not-rendered 'none';" +
+            //    "execution-while-out-of-viewport 'none';" +
+            //    "gyroscope 'none';" +
+            //    "magnetometer 'none';" +
+            //    "microphone 'none';" +
+            //    "midi 'none';" +
+            //    "navigation-override 'none';" +
+            //    "payment 'none';" +
+            //    "picture-in-picture 'none';" +
+            //    "publickey-credentials-get 'none';" +
+            //    "sync-xhr 'none';" +
+            //    "usb 'none';" +
+            //    "wake-lock 'none';" +
+            //    "xr-spatial-tracking 'none';"
+            //    ));
+            #endregion
+
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+
+            context.Response.Headers.Add("Content-Security-Policy", new StringValues(
+            "default-src 'self' 'unsafe-inline' https://maxcdn.bootstrapcdn.com https://fonts.gstatic.com https://fonts.googleapis.com ;" +
+            "img-src 'self' data: ;"
+
+            #region Other Directives that can be used
+            //"base-uri 'self';" +
+            //"block-all-mixed-content;" +
+            //"child-src 'none';" +
+            //"connect-src 'self';" +
+            //"font-src 'self' ;" +
+            //"form-action 'self' ;" +
+            //"frame-ancestors 'none';" +
+            //"frame-src 'none';" +
+            //"manifest-src 'none';" +
+            //"media-src 'none';" +
+            //"object-src 'self';" +
+            //"sandbox allow-scripts allow-forms;" +
+            //"script-src 'self';" +
+            //"script-src-elem 'self' 'unsafe-inline';" +
+            //"style-src 'self';" +
+            //"style-src-attr 'self' 'unsafe-inline';" +
+            //"style-src-elem 'self' 'unsafe-inline' https://maxcdn.bootstrapcdn.com  ;" +
+            //"upgrade-insecure-requests;"// +
+            //"worker-src 'self';"
+            #endregion
+
+            ));
+        }
+    return _next(context);
+    }
 }
