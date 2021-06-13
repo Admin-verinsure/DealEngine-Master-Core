41ccf4620687baba771cb44f2d1883f96aff817d
Ashu <ashu@techcertain.com>
2021-06-13 21:38:09 +1200

    director saving

--- DealEngine.Domain.Entities/OrganisationalUnit.cs
+++ DealEngine.Domain.Entities/OrganisationalUnit.cs
@@ -288,12 +288,12 @@ namespace DealEngine.Domain.Entities
             : base(User, Name, Type, Collection)
         {
         }
-        public virtual bool IsDirectorRetiredorDeceased { get; set; }
+        public virtual bool IsRetiredorDeceased { get; set; }
         [Display(Name = "Select to confirm you have received approval from the TripleA Advisers Association for this adviser to be included under the TripleA PI scheme")]
      
-      public virtual DateTime? DirectorDateofRetirement { get; set; }
+        public virtual DateTime? DateofRetirement { get; set; }
         [Display(Name = "Date of Deceased (Please Enter either Date of Retirement or Date of Deceased)")]
-        public virtual DateTime? DirectorDateofDeceased { get; set; }
+        public virtual DateTime? DateofDeceased { get; set; }
     }
 }
 
