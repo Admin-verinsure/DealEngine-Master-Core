9359b2f961fd60241bae6347bfdf4507881ea7ae
tcnathan <nathan@techcertain.com>
2021-09-08 10:22:56 +1200

    Remove subsystem from MoveAdvisors

--- DealEngine.WebUI/Controllers/InformationController.cs
+++ DealEngine.WebUI/Controllers/InformationController.cs
@@ -1525,27 +1525,27 @@ namespace DealEngine.WebUI.Controllers
                 await _programmeService.MoveAdvisorsToClientProgramme(advisors, clientProgramme, sourceClientProgramme, user);
 
                 //Clone or create sub uis
-                IList<SubClientProgramme> SubClientProgrammes;
-                if (clientProgramme.InformationSheet.NextInformationSheet.IsChange)
-                {
-                    SubClientProgrammes = clientProgramme.SubClientProgrammes;
-                    if (!SubClientProgrammes.Any())
-                    {
-                        SubClientProgrammes = clientProgramme.InformationSheet.NextInformationSheet.Programme.SubClientProgrammes;
-                    }
-                }
-                else
-                {
-                    SubClientProgrammes = clientProgramme.InformationSheet.NextInformationSheet.Programme.SubClientProgrammes;
-                }
-                if (SubClientProgrammes.Any())
-                {
-                    await _subsystemService.ValidateProgramme(clientProgramme.InformationSheet.NextInformationSheet, user);
-                }
-                else
-                {
-                    await _subsystemService.CreateSubObjects(clientProgramme.InformationSheet.NextInformationSheet.Programme.Id, clientProgramme.InformationSheet.NextInformationSheet, user);
-                }
+                //IList<SubClientProgramme> SubClientProgrammes;
+                //if (clientProgramme.InformationSheet.NextInformationSheet.IsChange)
+                //{
+                //    SubClientProgrammes = clientProgramme.SubClientProgrammes;
+                //    if (!SubClientProgrammes.Any())
+                //    {
+                //        SubClientProgrammes = clientProgramme.InformationSheet.NextInformationSheet.Programme.SubClientProgrammes;
+                //    }
+                //}
+                //else
+                //{
+                //    SubClientProgrammes = clientProgramme.InformationSheet.NextInformationSheet.Programme.SubClientProgrammes;
+                //}
+                //if (SubClientProgrammes.Any())
+                //{
+                //    await _subsystemService.ValidateProgramme(clientProgramme.InformationSheet.NextInformationSheet, user);
+                //}
+                //else
+                //{
+                //    await _subsystemService.CreateSubObjects(clientProgramme.InformationSheet.NextInformationSheet.Programme.Id, clientProgramme.InformationSheet.NextInformationSheet, user);
+                //}
 
                 //render new clientagreement doc
                 if (clientProgramme.InformationSheet.NextInformationSheet != null)
@@ -1561,7 +1561,6 @@ namespace DealEngine.WebUI.Controllers
                         var agreeTemplateList = newclientAgreement.Product.Documents;
                         var agreeDocList = newclientAgreement.GetDocuments();
 
-
                         //tripleA DO use case, remove when all client set as company
                         if (newclientAgreement.Product.Id == new Guid("bdbdda02-ee4e-44f5-84a8-dd18d17287c1") &&
                             newclientAgreement.ClientInformationSheet.Answers.Where(sa => sa.ItemName == "DAOLIViewModel.HasDAOLIOptions").First().Value == "2")
@@ -1570,7 +1569,6 @@ namespace DealEngine.WebUI.Controllers
                         }
                         else
                         {
-
                             if (!newclientAgreement.Product.IsOptionalCombinedProduct)
                             {
                                 foreach (SystemDocument template in agreeTemplateList)
@@ -1629,39 +1627,37 @@ namespace DealEngine.WebUI.Controllers
                                         }
 
                                         //render all subsystem
-                                        if (template.DocumentType == 6)
-                                        {
-                                            foreach (var subSystemClient in clientProgramme.InformationSheet.NextInformationSheet.SubClientInformationSheets)
-                                            {
-                                                if (newclientAgreement.Product.IsOptionalProductBasedSub)
-                                                {
-                                                    if (subSystemClient.Answers.Where(sa => sa.ItemName == newclientAgreement.Product.OptionalProductRequiredAnswer).First().Value == "1")
-                                                    {
-                                                        SystemDocument renderedDocSub = await _fileService.RenderDocument(user, template, newclientAgreement, subSystemClient, null);
-                                                        renderedDocSub.OwnerOrganisation = newclientAgreement.ClientInformationSheet.Owner;
-                                                        newclientAgreement.Documents.Add(renderedDocSub);
-                                                        documents.Add(renderedDocSub);
-                                                        await _fileService.UploadFile(renderedDocSub);
-                                                    }
-                                                }
-                                                else
-                                                {
-                                                    SystemDocument renderedDoc = await _fileService.RenderDocument(user, template, newclientAgreement, subSystemClient, null);
-                                                    renderedDoc.OwnerOrganisation = newclientAgreement.ClientInformationSheet.Owner;
-                                                    newclientAgreement.Documents.Add(renderedDoc);
-                                                    documents.Add(renderedDoc);
-                                                    await _fileService.UploadFile(renderedDoc);
-                                                }
-
-                                            }
-                                        }
+                                        //if (template.DocumentType == 6)
+                                        //{
+                                        //    foreach (var subSystemClient in clientProgramme.InformationSheet.NextInformationSheet.SubClientInformationSheets)
+                                        //    {
+                                        //        if (newclientAgreement.Product.IsOptionalProductBasedSub)
+                                        //        {
+                                        //            if (subSystemClient.Answers.Where(sa => sa.ItemName == newclientAgreement.Product.OptionalProductRequiredAnswer).First().Value == "1")
+                                        //            {
+                                        //                SystemDocument renderedDocSub = await _fileService.RenderDocument(user, template, newclientAgreement, subSystemClient, null);
+                                        //                renderedDocSub.OwnerOrganisation = newclientAgreement.ClientInformationSheet.Owner;
+                                        //                newclientAgreement.Documents.Add(renderedDocSub);
+                                        //                documents.Add(renderedDocSub);
+                                        //                await _fileService.UploadFile(renderedDocSub);
+                                        //            }
+                                        //        }
+                                        //        else
+                                        //        {
+                                        //            SystemDocument renderedDoc = await _fileService.RenderDocument(user, template, newclientAgreement, subSystemClient, null);
+                                        //            renderedDoc.OwnerOrganisation = newclientAgreement.ClientInformationSheet.Owner;
+                                        //            newclientAgreement.Documents.Add(renderedDoc);
+                                        //            documents.Add(renderedDoc);
+                                        //            await _fileService.UploadFile(renderedDoc);
+                                        //        }
+                                        //    }
+                                        //}
                                     }
                                 }
                             }
                         }
                     }
                 }
-
             }
             catch (Exception ex)
             {
