f0c1064ea0de53000b1e4b3c453c02417a0c65cf
Ashu Chauhan <ashu@techcertain.com>
2024-05-30 01:23:38 +0000

    Update HomeController.cs

--- DealEngine.WebUI/Controllers/HomeController.cs
+++ DealEngine.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿#region Using
+#region Using
 using AutoMapper;
 using ClosedXML.Excel;
 using DealEngine.Domain.Entities;
@@ -4084,7 +4084,7 @@ namespace DealEngine.WebUI.Controllers
                             await _programmeService.Update(clientProgramme);
 
                             //send out login instruction email
-                            await _emailService.SendSystemEmailLogin(email);
+                           // await _emailService.SendSystemEmailLogin(email);
                             //send out information sheet instruction email
                             EmailTemplate emailTemplate = programme.EmailTemplates.FirstOrDefault(et => et.Type == "SendInformationSheetReminder");
                             if (emailTemplate != null)
